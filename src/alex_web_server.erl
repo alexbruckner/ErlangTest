0debc0e84f650b43a6012ab47aa6c4f60990682b
Alex Bruckner <alex.bruckner@semantico.com>
2013-07-20 18:33:39 +0100

    some request url checking

--- src/alex_web_server.erl
+++ src/alex_web_server.erl
@@ -40,10 +40,16 @@ content(Request) ->
   Url = http_uri:decode(string:substr(Request, string:str(Request, " "))),
   CastOrCall = string:substr(Url, 3, 4),
   ActionString = string:substr(Url, string:str(Url, "?") + 1),
-  Action = string:substr(ActionString, 1, string:str(ActionString, "=") - 1),
+  Action = action(ActionString, string:str(ActionString, "=")),
+
+  io:format("Cast: Action=~p~n", [Action]),
+
   KeyValuePairs = string:substr(ActionString, string:str(ActionString, "=") + 1),
   return(list_to_atom(CastOrCall), list_to_atom(Action), string:strip(KeyValuePairs)).
 
+action(ActionString, Index) when Index > 0 ->
+  string:substr(ActionString, 1, Index - 1);
+action(ActionString, _) -> string:strip(ActionString).
 
 %% example: http://localhost:8080/cast?test=muh:value1,%20key2%20:%20%22value%202%22
 return(cast, Action, KeyValuePairs) ->
@@ -61,14 +67,20 @@ return(call, Action, KeyValuePairs) ->
 return(_, _Action, _KeyValuePairs) ->
   "context must be <cast> or <call>".
 
+
 tokenize(String) ->
   Tokens = string:tokens(String, ","),
-  [ to_tuple(X) || X <- Tokens].
+  [ to_tuple(X, string:str(X, ":")) || X <- Tokens].
 
-to_tuple(X) ->
-  K = list_to_atom(string:strip(string:substr(X, 1, string:str(X, ":") - 1))),
-  V = string:strip(string:substr(X, string:str(X, ":") + 1)),
-  {K,V}.
+to_tuple(X, Index) when Index > 0 ->
+  K = list_to_atom(string:strip(string:substr(X, 1, Index - 1))),
+  V = string:strip(string:substr(X, Index + 1)),
+  {K,V};
+to_tuple(_, _) -> {}.
+
+
+
+
 
 
 
